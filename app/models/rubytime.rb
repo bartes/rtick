96d7d4f20a96e0577ec8a7469031552e1f7b6fec
bartes <bartosz.knapik@llp.pl>
2010-12-02 00:14:29 +0100

    make it working

--- app/models/rubytime.rb
+++ app/models/rubytime.rb
@@ -5,18 +5,47 @@ class Rubytime < ActiveRecord::Base
   validates :password, :presence => true
   validates :login, :presence => true
   
-  def self.sign_in(agent, rubytime)
+  def sign_in
+    agent = Mechanize.new
     page = agent.get('http://rt.llp.pl/login')
     # Fill out the login form
     form = page.forms.detect{|f| f.action == '/login'}
     raise "Form /login not found!" unless form
-    form.login = rubytime.login
-    form.password = rubytime.password
+    form.login = login
+    form.password = password
     agent.submit(form)
+    agent
   end
   
-  def sign_in
-    agent = Mechanize.new
-    self.class.sign_in(agent, self)
+  def self.get_all_from_month(agent, mon, year) 
+    timer = Time.zone.local(year, mon)
+      
+    page = agent.get(:url => 'http://rt.llp.pl/activities', :params => {
+      :"search_criteria[date_from]" => format_date(timer.beginning_of_month),
+      :"search_criteria[date_to]"   => format_date(timer.end_of_month)
+    })
+    
+    parser = page.parser
+    
+    result = []
+
+    activities = parser.css(".activities tr")
+    activities.shift
+    activities.pop
+
+    activities.each do |a|
+      tds = a.css("td")
+      if tds.size == 3
+        result << {:date => tds.first.text, :time => tds[1].text}
+      else
+        result.last[:text] = tds.css("p").last.text
+      end 
+    end
+    result
+  end
+
+
+  def self.format_date(date)
+    date.to_date.to_s(:db).split("-").reverse.join("-")
   end
 end
--- app/models/tickspot.rb
+++ app/models/tickspot.rb
@@ -1,25 +1,74 @@
 require "mechanize"
+require "net/http"
 
 class Tickspot < ActiveRecord::Base
 
   validates :password, :presence => true
   validates :login, :presence => true
 
-  def self.sign_in(agent, tickspot)
-    page = agent.get('https://truvolabs.tickspot.com/login')
-
-    form = page.forms.detect{|f| f.action == '/login'}
-    raise "Form /login not found!" unless form
-    form.user_login = tickspot.login
-    form.user_password = tickspot.password
-
-    agent.submit(form)
-  end
+  DOMAIN = "truvolabs.tickspot.com"
 
   def sign_in
     agent = Mechanize.new
-    self.class.sign_in(agent, self)
+    page = agent.get('ogin')
+
+    form = page.forms.detect{|f| f.action == "https://#{self.class::DOMAIN}/login"}
+    raise "Form /login not found!" unless form
+    form.user_login = login
+    form.user_password = password
+
+    agent.submit(form)
+    agent
+  end
+
+  def self.format_time(hours, minutes)
+    sprintf('%02d:%02d', hours, minutes)
+  end
+
+  def api_request(path, params = {})
+    request = Net::HTTP::Post.new("/api/" + path)
+    request.form_data = {
+        'email' => login,
+        'password' => password
+    }.merge(params)
+
+    result = nil
+    Net::HTTP.new(self.class::DOMAIN).start {|http|
+      response = http.request(request)
+      result = response.body
+      code = response.code.to_i
+      raise "Request failed with code: #{code} and message #{response.body}" unless code < 300 && code >= 200
+    }
+    result
   end
 
 
+  def update_tickspot(data)
+    txt = api_request('projects', :open => true)
+    #doc = Nokogiri::XML.parse(txt)
+    #clients = parse_tickspot_clients(doc)
+    #client_id, client = get_tickspot_selected_id(config, clients.values, 'Client')
+    #project_id, project = get_tickspot_selected_id(config, client[:projects], 'Project')
+    #task_id, task = get_tickspot_selected_id(config, project[:tasks], 'Task')
+
+    api_request( 'create_entry', :task_id => task_id, :hours => data[:time], :date => data[:date], :notes => data[:text])
+  end
+   
+  def parse_tickspot_clients(doc)
+    clients = {}
+    doc.css('project').each do |project_elem|
+      client = parse_client(project_elem)
+      clients[client[:id]] ||= client
+      project = parse_project(project_elem)
+      clients[client[:id]][:projects] << project
+      project_elem.css('task').each do |task|
+        project[:tasks] << parse_task(task)
+      end
+    end
+    clients
+  end
+
+  def task_id
+    317764.to_s
+  end 
 end
